7cc39af2de82ab0393b54fedd5fa2bb60f072cc1
opilane <opilane@HP-CZC0359NP1>
2024-05-16 09:16:56 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("poo?");
+     
+            int poop;
+            poop = int.Parse(Console.ReadLine());
+            if (poop > 1)
+            {
+                Console.WriteLine("poopoopoo");
+            }
+            else
+            {
+                Console.WriteLine("poop");
+            }
+        }
+    }
+}
